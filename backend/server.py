6fcbf4470d91020e05c67458f31a0646e93ec568
apargupta9760708127-web <apargupta9760708127@gmail.com>
2026-06-22 00:58:39 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -126,13 +126,7 @@ async def get_youtube_channel():
 # Include the router in the main app
 app.include_router(api_router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 # Configure logging
 logging.basicConfig(
